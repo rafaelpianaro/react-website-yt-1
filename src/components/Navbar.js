0141b3a044322cb9a63839cc1431c79d9445e54f
Rafael Pianaro <rafaelpianaro@gmail.com>
2020-10-21 17:27:18 -0300

    added fontawesome into index.html and impor Link ro Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,18 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 function NavBar() {
-    return (
-        <>
-            <nav className="navbar">
-                <div className="navbar-container">
-
-                </div>
-            </nav>
-        </>
-    )
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo">
+            
+          </Link>
+        </div>
+      </nav>
+    </>
+  )
 }
 
 export default NavBar
