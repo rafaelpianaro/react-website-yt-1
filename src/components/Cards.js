08aff505482f64f70bc79bad8cfea0902b2e304f
Rafael Pianaro <rafaelpianaro@gmail.com>
2020-10-21 22:34:39 -0300

    adde Home, CardItem and Cards with styles

---
+++ src/components/Cards.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import CardItem from './CardItem'
+import './Cards.css'
+
+function Cards() {
+	return (
+		<div className="cards">
+			<h1>Check out these EPIC Destinations!</h1>
+			<div className="cards__container">
+				<ul className="cards__items">
+					<CardItem 
+					src="images/img-9.jpg"
+					text="Explore the hidden waterfall deep insite the Amazon Jungle"
+					label="Adventure"
+					path="/services"
+					/>
+					<CardItem 
+					src="images/img-2.jpg"
+					text="Travel through the Islands of Bali in a Private Cruise"
+					label="Luxury"
+					path="/services"
+					/>
+				</ul>
+			</div>
+		</div>
+	)
+}
+
+export default Cards
